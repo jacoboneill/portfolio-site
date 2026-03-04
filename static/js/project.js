3a634bcf9b29b92a78e8b5c10053338171cccb3e
Jacob O'Neill <jacoboneill2000@outlook.com>
2026-03-04 18:40:26 +0000

    Change preview to use video rather than animated webp

--- static/js/project.js
+++ static/js/project.js
@@ -1,12 +1,36 @@
-document.querySelectorAll(".project").forEach((p) => {
-  const preview = p.querySelector("img");
+const getName = (img) => {
+  return img.src.split("/").pop().replace(".webp", "");
+};
 
-  p.addEventListener("mouseenter", () => {
-    preview.src =
-      preview.src.split("_static")[0] + "_dynamic.webp?t=" + Date.now();
+const newPreviewImage = (name) => {
+  const img = document.createElement("img");
+  img.className = "preview";
+  img.src = `/static/img/${name}.webp`;
+  img.alt = `${name} preview`;
+  img.addEventListener("mouseenter", () => {
+    img.replaceWith(newPreviewVideo(name));
   });
+  return img;
+};
 
-  p.addEventListener("mouseleave", () => {
-    preview.src = preview.src.split("_dynamic")[0] + "_static.webp";
+const newPreviewVideo = (name) => {
+  const video = document.createElement("video");
+  video.className = "preview";
+  video.autoplay = true;
+  video.loop = true;
+  video.muted = true;
+  video.playsInline = true;
+  video.src = `/static/video/${name}.webm`;
+  video.addEventListener("mouseleave", () => {
+    video.replaceWith(newPreviewImage(name));
+  });
+  return video;
+};
+
+document.querySelectorAll(".project").forEach((p) => {
+  const preview = p.querySelector(".preview");
+  const name = getName(preview);
+  preview.addEventListener("mouseenter", () => {
+    preview.replaceWith(newPreviewVideo(name));
   });
 });
